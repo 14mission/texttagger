a70f538fe10038a5c4fe6b0bd74f09f207c47ecd
Nick Kibre, PhD <nkibre@gmail.com>
2025-08-28 07:17:12 -0700

    improve formatting

--- scorepunc.py
+++ scorepunc.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 import sys
 for fn in sys.argv[1:]:
+
+  # read tsv file
+  # lines should be: id \t refcat \t hypcat
+  # build: typedispo->refcat->hypcat counts
   print(f"read {fn}")
   fh = open(fn)
   typedispo = {}
@@ -13,12 +17,21 @@ for fn in sys.argv[1:]:
       typedispo[reftag][hyptag] = 0
     typedispo[reftag]["tot"] += 1
     typedispo[reftag][hyptag] += 1
+
+  # longest refcat len
+  longestcatlen = 0
+  for cat in typedispo.keys():
+    if len(cat) > longestcatlen:
+      longestcatlen = len(cat)
+
+  # report types and dispos
+  print("refcat".ljust(longestcatlen,' ') + "\tn\trecall\thypcats")
   allcatscount = 0
   allcatscorrect = 0
   for typeanddispos in sorted(typedispo.items(), key=lambda item: str(1.0/item[1]["tot"])+item[0]): # sort by typetot desc, type
     type, dispos = typeanddispos
     cols = []
-    cols.append(type)
+    cols.append(type.ljust(longestcatlen,' '))
     cols.append(str(dispos["tot"]))
     catcount = dispos["tot"]
     catcorrectcount = dispos[type] if type in dispos else 0
@@ -30,4 +43,4 @@ for fn in sys.argv[1:]:
       if dispo == "tot": continue
       cols.append(dispo+"="+str(count))
     print("\t".join(cols))
-  print("ALL\t"+str(allcatscount)+"\t"+str(int(100*allcatscorrect/allcatscount+0.5))+"%\tself="+str(allcatscorrect)+"\tother="+str(allcatscount-allcatscorrect))
+  print("ALL".ljust(longestcatlen,' ')+"\t"+str(allcatscount)+"\t"+str(int(100*allcatscorrect/allcatscount+0.5))+"%\tself="+str(allcatscorrect)+"\tother="+str(allcatscount-allcatscorrect))
